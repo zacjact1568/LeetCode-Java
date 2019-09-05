08750692f94621cf635b639bf60509ff70b82fa0
zacjact1568 <511470661@qq.com>
2019-09-05 18:52:22 +0800

    23# 合并K个排序链表

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 |8|[String to Integer (atoi)](https://leetcode.com/problems/string-to-integer-atoi/)|[StringToInteger.java](src/net/zackzhang/code/leetcode/problems/StringToInteger.java)|
 |9|[Palindrome Number](https://leetcode.com/problems/palindrome-number/)|[PalindromeNumber.java](src/net/zackzhang/code/leetcode/problems/PalindromeNumber.java)|
 |17|[电话号码的字母组合](https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/)|[LetterCombinationsOfAPhoneNumber.java](src/net/zackzhang/code/leetcode/problems/LetterCombinationsOfAPhoneNumber.java)|
+|23|[合并K个排序链表](https://leetcode-cn.com/problems/merge-k-sorted-lists/)|[MergeKSortedLists.java](src/net/zackzhang/code/leetcode/problems/MergeKSortedLists.java)|
 |771|[Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/)|[JewelsAndStones.java](src/net/zackzhang/code/leetcode/problems/JewelsAndStones.java)|
 |807|[Max Increase to Keep City Skyline](https://leetcode.com/problems/max-increase-to-keep-city-skyline/)|[MaxIncreaseToKeepCitySkyline.java](src/net/zackzhang/code/leetcode/problems/MaxIncreaseToKeepCitySkyline.java)|
 |938|[Range Sum of BST](https://leetcode.com/problems/range-sum-of-bst/)|[RangeSumOfBST.java](src/net/zackzhang/code/leetcode/problems/RangeSumOfBST.java)|
---
+++ src/net/zackzhang/code/leetcode/problems/MergeKSortedLists.java
@@ -0,0 +1,66 @@
+package net.zackzhang.code.leetcode.problems;
+
+/** 23# 合并K个排序链表 */
+public class MergeKSortedLists {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    private ListNode solution(ListNode[] lists) {
+        int len = lists.length;
+        if (len == 0) return null;
+        // 步长（窗口长度）
+        int step = 1;
+        while (step < len) {
+            for (int i = 0; i < len - step; i += step * 2) {
+                // 将合并后链表的头节点放到窗口的第一个位置
+                lists[i] = merge(lists[i], lists[i + step]);
+            }
+            step *= 2;
+        }
+        return lists[0];
+    }
+
+    // 合并两个排序链表
+    private ListNode merge(ListNode head1, ListNode head2) {
+        // 辅助头节点
+        ListNode head = new ListNode(0);
+        // 当前的尾节点
+        ListNode tail = head;
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        // 此时，head1 和 head2 最多只可能有一个为 null
+        tail.next = head1 != null ? head1 : head2;
+        return head.next;
+    }
+
+    public static void test() {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(4);
+        ListNode a3 = new ListNode(5);
+        a1.next = a2;
+        a2.next = a3;
+        ListNode b1 = new ListNode(1);
+        ListNode b2 = new ListNode(3);
+        ListNode b3 = new ListNode(4);
+        b1.next = b2;
+        b2.next = b3;
+        ListNode c1 = new ListNode(2);
+        ListNode c2 = new ListNode(6);
+        c1.next = c2;
+        System.out.println(new MergeKSortedLists().solution(new ListNode[]{a1, b1, c1}));
+    }
+}
